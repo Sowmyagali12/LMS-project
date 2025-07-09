a397add69b0e85483996f9567d2318e3750abe38
Gali Sowmya <140953235+Sowmyagali12@users.noreply.github.com>
2025-07-09 16:01:44 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
@@ -251,6 +252,11 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+
+
+
 
 
 

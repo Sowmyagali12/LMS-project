59db8941f2a18031b6015877b5196320af563f38
Gali Sowmya <140953235+Sowmyagali12@users.noreply.github.com>
2025-07-11 14:01:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import AboutUs from './pages/AboutUs';
+// Pages
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
 import ChangePassword from './pages/ChangePassword';
+import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import Courses from './pages/Courses';
-import CourseRegistration from './pages/CourseRegistration';
-import PaymentPage from './pages/PaymentPage';
 import CourseLanding from './pages/CourseLanding';
-
+import EnrolledCourses from './pages/EnrolledCourses';
+import CourseContent from './pages/CourseContent';
 
 function App() {
   return (
     <Router>
-      
-      <Navbar />
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar always visible */}
+        <Navbar />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/change-password" element={<ChangePassword/>}/>
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/courses" element={<Courses />} />
-         <Route path="/aboutus" element={<AboutUs />} />
-         <Route path="/course-registration" element={<CourseRegistration />} />
-         <Route path="/payment" element={<PaymentPage/>} />
-          {/* <Route path="/courses/:courseId" element={<CourseLanding />} /> */}
-          <Route path="/courses/:courseId" element={<CourseLanding />} />
+        {/* Main content area */}
+        <main className="flex-grow">
+          <Routes>
+            {/*  Public Routes */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contactus" element={<ContactUs />} />
 
-          
-        </Routes>
-      </main>
+            {/* Course-Related Routes */}
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/courses/:courseId" element={<CourseLanding />} />
+            <Route path="/dashboard" element={<EnrolledCourses />} />
+            <Route path="/course-content/:courseId" element={<CourseContent />} />
+          </Routes>
+        </main>
 
-    
-      <Footer />
-      
+        {/* Footer always visible */}
+        <Footer />
+      </div>
     </Router>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
